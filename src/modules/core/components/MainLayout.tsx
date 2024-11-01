030fda19b626e559e79cc5b668045443e8c60838
Djkde01 <sergio.estrella@utp.edu.co>
2024-11-01 16:38:18 -0500

    feat: add ChallengeLayout component and integrate it into MainLayout

--- src/modules/core/components/MainLayout.tsx
+++ src/modules/core/components/MainLayout.tsx
@@ -1,21 +1,31 @@
+import ChallengeLayout from "@/modules/challenge/components/ChallengeLayout"
 import Header from "../design-system/Header"
 
 function MainLayout() {
     return (
+
+
         <div className="bg-primary min-h-screen p-4">
 
             <Header />
-            <section className="mb-6">
-                <h2 className="title-small">Retos de cursos actuales</h2>
-                <button className="btn-secondary mt-2">Ya disponible</button>
-            </section>
+            <ChallengeLayout />
+            {
 
-            <section>
-                <h2 className="title-small">Retos por escuelas</h2>
-                <button className="btn-secondary mt-2">Ya disponible</button>
-                <p>Ejemplo de P</p>
-            </section>
 
+                /**
+                    <section className="mb-6">
+                        <h2 className="title-small">Retos de cursos actuales</h2>
+                        <button className="btn-secondary mt-2">Ya disponible</button>
+                    </section>
+        
+                    <section>
+                        <h2 className="title-small">Retos por escuelas</h2>
+                        <button className="btn-secondary mt-2">Ya disponible</button>
+                        <p>Ejemplo de P</p>
+                    </section>
+        
+                    */
+            }
         </div>
 
     )
