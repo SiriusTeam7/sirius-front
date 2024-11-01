947aec70583e59ce11001059069365ce185ed37f
Djkde01 <sergio.estrella@utp.edu.co>
2024-11-01 14:37:54 -0500

    refactor: restructure App component and introduce MainLayout with Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
 import '../src/styles/styles.css'
+import MainLayout from './modules/core/components/MainLayout';
 const App = () => {
   return (
-    <div className="bg-primary min-h-screen p-4">
-      <header className="mb-6">
-        <h1 className="title-large">Sirius: Memoria Espaciada</h1>
-        <input
-          type="text"
-          placeholder="¿Qué te gustaría aprender hoy?"
-          className="input-search"
-        />
-      </header>
-
-      <section className="mb-6">
-        <h2 className="title-small">Retos de cursos actuales</h2>
-        <button className="btn-secondary mt-2">Ya disponible</button>
-      </section>
-
-      <section>
-        <h2 className="title-small">Retos por escuelas</h2>
-        <button className="btn-secondary mt-2">Ya disponible</button>
-        <p>Ejemplo de P</p>
-      </section>
-    </div>
+    <MainLayout />
   );
 };
 
