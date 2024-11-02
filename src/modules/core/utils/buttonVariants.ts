f6664d4c49108549d8b232ce8b30c3bf928b9186
Djkde01 <sergio.estrella@utp.edu.co>
2024-11-02 16:25:02 -0500

    feat: refactor Button component and extract buttonVariants to a separate file; add breathing button variant and update styles

--- src/modules/core/utils/buttonVariants.ts
+++ src/modules/core/utils/buttonVariants.ts
@@ -12,12 +12,14 @@ export const buttonVariants = cva(
         subtle: "bg-slate-100 text-slate-900 hover:bg-slate-200",
         ghost: "bg-transparent hover:bg-slate-100 text-slate-800",
         link: "bg-transparent underline-offset-4 hover:underline text-[#2B9FE6]",
+        breathing:
+          "relative bg-[#1E1B1B] text-white hover:bg-[#2B9FE6] flex items-center justify-center rounded-full breathing-button",
       },
       size: {
         default: "h-10 py-2 px-4",
         sm: "h-9 px-3 rounded-md",
         lg: "h-11 px-8 rounded-md",
-        icon: "h-10 w-10 rounded-full",
+        icon: "size-11 rounded-full",
       },
     },
     defaultVariants: {
