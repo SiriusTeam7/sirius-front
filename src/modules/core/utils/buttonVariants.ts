f6ff430bc6a8da81795f8058fce39ddb46337955
Djkde01 <sergio.estrella@utp.edu.co>
2024-11-03 01:22:18 -0500

    feat: enhance breathing button variant styles and update Tailwind CSS purge configuration

--- src/modules/core/utils/buttonVariants.ts
+++ src/modules/core/utils/buttonVariants.ts
@@ -13,7 +13,7 @@ export const buttonVariants = cva(
         ghost: "bg-transparent hover:bg-slate-100 text-slate-800",
         link: "bg-transparent underline-offset-4 hover:underline text-[#2B9FE6]",
         breathing:
-          "relative bg-[#1E1B1B] text-white hover:bg-[#2B9FE6] flex items-center justify-center rounded-full breathing-button",
+          "relative bg-[#1E1B1B] text-white hover:bg-[#2B9FE6] inline-flex items-center justify-center p-4 rounded-full breathing-button",
       },
       size: {
         default: "h-10 py-2 px-4",
