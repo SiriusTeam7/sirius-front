d5febaf3d7de053a115461c117105d3f611d2645
sarciniegas17 <sarciniegas@indracompany.com>
2024-11-16 16:40:05 -0500

    add redux for authenticater

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import '../src/styles/styles.css'
 import MainLayout from './modules/core/components/MainLayout';
 const App = () => {
   return (
+
     <MainLayout />
   );
 };
 
 export default App;
+ 
