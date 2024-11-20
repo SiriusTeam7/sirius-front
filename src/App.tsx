852b7fcd21429ca0b98a2136e8f305b0b0005680
Djkde01 <sergio.estrella@utp.edu.co>
2024-11-20 18:56:38 -0500

    fix: correct casing of Login component import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "../src/styles/styles.css";
 import MainLayout from "./modules/core/components/MainLayout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./modules/login/components/login";
+import Login from "./modules/login/components/Login";
 const App = () => {
   return (
     <Router>
