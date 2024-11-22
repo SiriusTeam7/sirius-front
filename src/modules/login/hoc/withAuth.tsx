0d9107e2df2988812cbb4e4cea63aab69d41e9a9
Djkde01 <sergio.estrella@utp.edu.co>
2024-11-21 23:53:04 -0500

    refactor: enhance ProtectedRoute to handle loading state and improve session validation logic

--- src/modules/login/hoc/withAuth.tsx
+++ src/modules/login/hoc/withAuth.tsx
@@ -1,28 +1,32 @@
 import { Navigate, useNavigate } from "react-router-dom";
 import { useLogin } from "../hooks/useLogin";
-import React, { ReactElement, useEffect } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 
 interface WithProps {
     children: ReactElement;
 }
 
 export const ProtectedRoute: React.FC<WithProps> = ({ children }) => {
-
-    const [isAuth, setIsAuth] = React.useState<boolean>(false);
+    const [isAuth, setIsAuth] = useState<boolean | null>(null); // Use null to indicate loading state
     const navigate = useNavigate();
     const { validateSession } = useLogin();
 
     useEffect(() => {
         const checkAuth = async () => {
-            const isAuth = await validateSession();
-            setIsAuth(isAuth);
-            if (!isAuth) {
+            const authorized = await validateSession();
+            setIsAuth(authorized);
+            if (!authorized) {
                 navigate('/login');
             }
-        }
-        checkAuth();
+        };
 
-    }, [navigate]);
+        checkAuth();
+    }, [navigate, validateSession]);
+
+    if (isAuth === null) {
+        // Render a loading state while checking authentication
+        return <div>Loading...</div>;
+    }
 
     return isAuth ? children : <Navigate to="/login" />;
 };
--- src/modules/login/hooks/useLogin.tsx
+++ src/modules/login/hooks/useLogin.tsx
@@ -8,58 +8,37 @@ export function useLogin() {
   const [password, setPassword] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
-  const { mutate, isError, isSuccess } = useGetLogin();
-
+  const { mutate } = useGetLogin();
   const navigate = useNavigate();
-
 
   const handleSubmit = () => {
     if (username && password) {
       setIsLoading(true);
       const loginData: LoginRequest = { username, password };
       mutate(loginData, {
-
         onSuccess: () => {
           setIsLoading(false);
-          console.log('Login exitoso')
-          navigate('/');
+          navigate("/");
         },
         onError: (error) => {
           setIsLoading(false);
           setError(error.message);
         },
       });
-    }
-    else {
+    } else {
       setError("Por favor, llena todos los campos");
     }
   };
 
-
   const validateSession = (): boolean => {
     // Check if cookies are present
     const cookies = document.cookie.split(";");
-    console.log("🚀 ~ validateSession ~ cookies:", document)
-
     // Check if cookies "csrftoken" and "sessionid" are present
-    const csrfToken = cookies.find((cookie) => cookie.includes("csrftoken"));
-    console.log("🚀 ~ csrfToken:", csrfToken);
-    const sessionId = cookies.find((cookie) => cookie.includes("sessionid"));
-    console.log("🚀 ~ sessionId:", sessionId);
-    const refresh_token = cookies.find((cookie) => cookie.includes("refresh_token"));
-    console.log("🚀 ~ refresh_token:", refresh_token);
+    const csrfToken = cookies.some((cookie) => cookie.includes("csrftoken"));
+    const sessionId = cookies.some((cookie) => cookie.includes("sessionid"));
 
-    // If both cookies are present, and values are not empty, and expiration date is in the future, return true
-    if (
-      csrfToken &&
-      sessionId &&
-      csrfToken.split("=")[1] &&
-      sessionId.split("=")[1]
-    ) {
-      return true;
-    }
-    return false;
-  }
+    return csrfToken && sessionId;
+  };
 
   return {
     username,
@@ -67,10 +46,8 @@ export function useLogin() {
     password,
     setPassword,
     handleSubmit,
-    validateSession,
     isLoading,
-    isError,
-    isSuccess,
     error,
+    validateSession,
   };
 }
