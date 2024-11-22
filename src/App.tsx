ba8fc492c1975e037ea481b9777c4e47b6949b6d
Djkde01 <sergio.estrella@utp.edu.co>
2024-11-21 23:28:17 -0500

    refactor: implement ProtectedRoute for MainLayout and remove unused authSlice

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import "../src/styles/styles.css";
 import MainLayout from "./modules/core/components/MainLayout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./modules/login/components/Login";
+import { ProtectedRoute } from "./modules/login/hoc/withAuth";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<MainLayout />} />
+        <Route path="/" element={<ProtectedRoute><MainLayout /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
