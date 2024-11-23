eb7e778eb03711bd32a45b18824fbaf304686440
Djkde01 <sergio.estrella@utp.edu.co>
2024-11-22 23:23:03 -0500

    feat: add ChartArea SVG and update FactsLayout to include leaderboard link

--- src/modules/home/components/FactsCard.tsx
+++ src/modules/home/components/FactsCard.tsx
@@ -1,28 +1,32 @@
+import { twMerge } from "tailwind-merge";
+
 interface FactCardProps {
   title: string;
   subtitle: string;
   icon: string;
-  iconBgColor: string;
+  iconColor: string;
 }
 
-const   FactCard = ({
+const FactCard = ({
   title,
   subtitle,
   icon,
-  iconBgColor,
+  iconColor,
+
 }: FactCardProps) => {
   return (
-    <div className="flex items-center w-64 p-2  bg-[#1B1F24] border border-gray-600 rounded-lg">
+    <div className="flex items-center w-64 p-2 border border-gray-600 rounded-lg bg-[#1B1F24]">
       <div
-        className={`flex items-center justify-center w-12 h-12 bg-[${iconBgColor}] rounded-md`}
+        className={twMerge("flex items-center justify-center w-12 h-12 rounded-md")}
+        style={{ backgroundColor: iconColor }}
       >
-       <img src={icon} className="w-6 h-12" />
+        <img src={icon} className="w-6 h-12" />
       </div>
       <div className="ml-4">
         <div className="text-white font-semibold">{title}</div>
         <div className="text-gray-400">{subtitle}</div>
       </div>
-    </div>
+    </div >
   );
 };
 
