6a81df320032974eb1eba355f6525fc1d4949af6
Djkde01 <sergio.estrella@utp.edu.co>
2024-11-22 22:07:38 -0500

    feat: add leaderboard route with protected access in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import MainLayout from "./modules/core/components/MainLayout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./modules/login/components/Login";
 import { ProtectedRoute } from "./modules/login/hoc/withAuth";
+import LeaderboardLayout from "./modules/leaderboard/components/LeaderboardLayout";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<ProtectedRoute><MainLayout /></ProtectedRoute>} />
+        <Route path="/leaderboard" element={<ProtectedRoute><LeaderboardLayout /></ProtectedRoute>} />
+        <Route path="*" element={<ProtectedRoute><MainLayout /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
